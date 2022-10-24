fc9d6b115ea801da4b3e3a3a002992f87938c0a9
dlwesemann <101487454+dlwesemann@users.noreply.github.com>
2022-10-24 17:19:53 -0600

    initial customization

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Updates</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
-        Slow-carb messenger bag mlkshk fingerstache four dollar toast.
+        SDI Analytics Development Updates
       </h2>
     </div>
 
@@ -46,7 +46,7 @@ const Pagination = () => import('@/components/commons/pagination.vue');
 
   head(): MetaInfo {
     return {
-      title: 'Blog',
+      title: 'Updates',
       meta: [
         {
           hid: 'description',
